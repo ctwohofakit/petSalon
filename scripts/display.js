82b2a69aa34bf47a9464c7ff16a460de6f047030
ctwohofakit <ctwohofa@gmail.com>
2024-11-22 23:07:55 -0800

    Final Report

--- scripts/display.js
+++ scripts/display.js
@@ -8,12 +8,13 @@
         let pet = pets[i];
         //display card script
         result += `
+        <div id="${i}" class="card col-3 mx-2">
         <div class="card col-3 mx-2" style="width: 18rem;">
         <div class="card-body">
             <h5 class="card-title">${pet.name}</h5>
             <h6 class="card-subtitle mb-2 text-body-secondary">${pet.breed}</h6>
             <p class="card-text">${pet.gender =="Male"? 'Male':'Female'},${pet.age} years</p>
-            <button class="btn btn-danger">Delete</button>
+            <button class="btn btn-danger" onclick="deletePet(${i});">Delete</button>
         </div>
         </div>`
 
@@ -26,7 +27,7 @@
 
 //table view display
 function displayRow(){
-    let card = document.getElementById("pets");
+    let table = document.getElementById("pets");
     let result=`
         <table class="table table-sm table-dark">
         <thead>
@@ -38,30 +39,82 @@ function displayRow(){
                 <th scope="col">Gender</th>
                 <th scope="col">Service</th>
                 <th scope="col">Type</th>
+                <th scope="col">Color</th>
+                <th scope="col">Action</th>
             </tr>
         </thead>
         <tbody id="petRows">
         </tbody>
         </table>
     `;
-    card.innerHTML =result;
+    table.innerHTML =result;
 
     let petRows=document.getElementById("petRows");
     let rows ="";
     for(let i=0; i<pets.length; i++){
         let pet = pets[i];
         rows+=`
-            <tr>
-                <th scope="row">${i}</th>
-                <td>${pet.name}</td>
-                <td>${pet.breed}</td>
-                <td>${pet.age}</td>
-                <td>${pet.gender}</td>
-                <td>${pet.service}</td>
-                <td>${pet.type}</td>
-            </tr>
+            <table id="registerTable" class="table table-sm table-dark">
+            <thead>
+                <tr id="${i}">
+                    <th scope="row">${i}</th>
+                    <td>${pet.name}</td>
+                    <td>${pet.breed}</td>
+                    <td>${pet.age}</td>
+                    <td>${pet.gender}</td>
+                    <td>${pet.service}</td>
+                    <td>${pet.type}</td>
+                    <td>${pet.color}</td>
+                    <td><button class="btn btn-danger"onclick="deletePet(${i});">Delete</button></td>
+                </tr>
+            </thead>
+            </table>
         `;
     }
     petRows.innerHTML = rows;
+    displayInfo()
 
+}
+
+
+
+
+
+
+
+function displayInfo(){
+let totalDiv =  document.getElementById("total");
+let groomingDiv = document.getElementById("gTotal");
+let vaccinesDiv = document.getElementById("vTotal");
+let nailDiv = document.getElementById("nTotal");
+
+let grooming=0;
+let vaccines=0;
+let nails=0;
+
+
+totalDiv.innerHTML= pets.length;// total pets
+
+    for(let i=0; i<pets.length; i++){
+        console.log(pets[i].service); 
+
+        if(pets[i].service=="Grooming"){//grooming
+            grooming++;
+            groomingDiv.innerHTML=grooming;
+
+        }else if(pets[i].service=="Vaccines") {//Vaccines
+            vaccines++;
+            vaccinesDiv.innerHTML=vaccines;
+
+        }else{//nails
+            nails++;
+            nailDiv.innerHTML=nails;
+
+        }
+    }
+    // Update with recalculated counts for delete pet
+    totalDiv.innerHTML = pets.length;
+    groomingDiv.innerHTML = grooming;
+    vaccinesDiv.innerHTML = vaccines;
+    nailDiv.innerHTML = nails;
 }
--- scripts/register.js
+++ scripts/register.js
@@ -9,13 +9,14 @@ let petSalon ={
 }
 
 //object constructor
-function Pet(name, age, gender, breed, service, type){
+function Pet(name, age, gender, breed, service, type, color){
     this.name=name;
     this.age=age;
     this.gender=gender;
     this.breed=breed;
     this.service=service;
     this.type=type;
+    this.color=color;
 }
 
 //validation fucntion
@@ -27,41 +28,53 @@ function isValid(pet){
     let inputBreed= document.getElementById("txtBreed");
     let inputService= document.getElementById("txtService");
     let inputType= document.getElementById("txtType");
+    let inputColor= document.getElementById("txtColor");
 
     if(pet.name == ""){
         validation = false;
         inputName.classList.add("error");
+    }else{inputName.classList.remove("error");
 
     }
+
     if(pet.age == ""){
         validation = false;
         inputAge.classList.add("error");
+    }else{inputAge.classList.remove("error");
     }
     if(pet.gender == ""){
         validation = false;
         inputGender.classList.add("error");
+    }else{inputGender.classList.remove("error");
     }
     if(pet.breed == ""){
         validation = false;
         inputBreed.classList.add("error");
+    }else{inputBreed.classList.remove("error");
     }
     if(pet.service == ""){
         validation = false;
         inputService.classList.add("error");
+    }else{inputService.classList.remove("error");
     }
     if(pet.type == ""){
         validation = false;
         inputType.classList.add("error");
+    }else{inputType.classList.remove("error");
     }
-
-
-
-
-
-
+    if(pet.color == ""){
+        validation = false;
+        inputColor.classList.add("error");
+    }else{inputColor.classList.remove("error");
+    }
+    
 
     return validation;
 }
+
+
+
+
 
 //register fucntion
 function register(){
@@ -71,10 +84,11 @@ function register(){
     let inputBreed=document.getElementById("txtBreed").value;
     let inputService=document.getElementById("txtService").value;
     let inputType=document.getElementById("txtType").value;
-    console.log(inputName,inputAge,inputGender,inputBreed,inputService, inputType);
+    let inputColor=document.getElementById("txtColor").value;
+    console.log(inputName,inputAge,inputGender,inputBreed,inputService, inputType, inputColor);
 
     //create the obj
-    let newPet = new Pet (inputName,inputAge,inputGender,inputBreed,inputService, inputType);
+    let newPet = new Pet (inputName,inputAge,inputGender,inputBreed,inputService, inputType, inputColor);
     if (isValid(newPet)==true){
     
         //push the obj to the array
@@ -101,8 +115,21 @@ function clearForm(){
     document.getElementById("txtBreed").value="";
     document.getElementById("txtService").value="";
     document.getElementById("txtType").value="";
+    document.getElementById("txtColor").value="";
 }
 
+
+
+//delete fucntion
+function deletePet(petId){
+    console.log("Delete pets"+petId);
+    document.getElementById(petId).remove;
+    pets.splice(petId,1);//start point
+    
+    //displayPet();
+    displayRow();
+    displayInfo(petId);
+}
 
 
 
@@ -111,11 +138,11 @@ function clearForm(){
 
 function init(){
     //execution code should be inside of this fucntion
-    let pet1= new Pet("Scooby",99,"Male","Boxer","Grooming","Dog");
-    let pet2= new Pet("Mocha",2,"Male","Maltipoo","Grooming", "Dog");
-    let pet3= new Pet("Snowball",12,"Female","Mixed","Vaccines","Cat");
-
-    pets.push(pet1,pet2,pet3);
+    let pet1= new Pet("Scooby",15,"Male","Boxer","Grooming","Dog","Brown");
+    let pet2= new Pet("Mocha",2,"Male","Maltipoo","Grooming", "Dog","Gray");
+    let pet3= new Pet("Snowball",12,"Female","Mixed","Vaccines","Cat","Black");
+    let pet4= new Pet("ball",10,"Male","Mixed","Nails","Cat","White");
+    pets.push(pet1,pet2,pet3,pet4);
 
     //displayPet();
     displayRow();
